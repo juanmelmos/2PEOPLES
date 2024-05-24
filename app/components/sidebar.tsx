a9fa360689364185f606b3f406973583c47b6d12
Juan Melguizo <juamelmos@alu.edu.gva.es>
2024-05-24 19:46:46 +0200

    Style: Apply a better style to the sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -5,43 +5,42 @@ import Link from "next/link";
 import style from "../ui/sidebar.module.css";
 import styleHeader from "../ui/page.module.css";
 import { usePathname } from "next/navigation";
-import { getId } from "../../lib/data";
 import { logOut } from "@/lib/actions";
 
 
 export default function Sidebar({ idUser }: { idUser: number }) {
 
 
-  // const logged = getId() !== 0;
-
-
   const pathname = usePathname();
   return (
     <header className={styleHeader.sidebar}>
+      <div className={style.logoContainer}>
       <Image
+      className={style.logo}
         src="/logo.png"
         alt="Logo 2PEOPLES"
-        width={100}
-        height={100}
+        width={200}
+        height={200}
       />
+      </div>
       <nav>
         <ul>
           <li><Link
             href="/"
-            className={`${pathname === '/' ? style.here : style.nothere}`}
+            className={`${pathname === '/' ? style.here : style.nothere} ${style.link}`}
           >Home</Link></li>
           <li><Link
             href="/events"
-            className={`${pathname === '/events' ? style.here : style.nothere}`}
+            className={`${pathname === '/events' ? style.here : style.nothere} ${style.link}`}
           >Events</Link></li>
         </ul>
-        {idUser !== 0 ? 
+        {idUser !== 0 ?
         <form action={logOut}>
-          <button type="submit">LogOut</button>
+          <button type="submit" className={style.logOut}>LogOut</button>
         </form> : 
         <Link
           href="/login"
-          className={`${pathname === '/login' ? style.here : style.nothere}`}
+          className={`${pathname === '/login' ? style.here : style.nothere} ${style.link}`}
         >Log In</Link>}
       </nav>
     </header>
