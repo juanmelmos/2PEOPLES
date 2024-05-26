419dd55b5786b2f84d96d42d610b7f273e7eb73d
Juan Melguizo <juamelmos@alu.edu.gva.es>
2024-05-26 09:48:15 +0200

    Feat: Add the register link to the login and vice versa

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,13 @@ import "../ui/globals.css";
 import "../ui/page.module.css"
 import style from "../ui/login-register.module.css"
 import { checkLogin } from "../../lib/actions";
+import Link from "next/link";
 
 export default function Login() {
 
   return (
     <div>
+      <h2>Login</h2>
     <form action={checkLogin}>
       <label>
         Username:
@@ -22,6 +24,7 @@ export default function Login() {
       <br />
       <button type="submit">Enviar</button>
     </form>
+    <p>I don't have an account, <Link href="/register" className={style.link}>register</Link></p>
     </div>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,10 +4,12 @@ import "../ui/globals.css";
 import "../ui/page.module.css"
 import style from "../ui/page.module.css"
 import { register } from "../../lib/actions";
+import Link from "next/link";
 
 export default function Register() {
   return (
     <div>
+      <h2>Register</h2>
     <form action={register}>
       <label>
         Username:
@@ -20,6 +22,7 @@ export default function Register() {
       </label>
       <br />
       <button type="submit">Enviar</button>
+      <p>I have an account, <Link href="/register" className={style.link}>log in</Link></p>
     </form>
     </div>
   );
