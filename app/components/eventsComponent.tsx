a5e9d866d0327b44dbc365c98cf80034ab3740a5
Juan Melguizo <juamelmos@alu.edu.gva.es>
2024-05-26 10:17:43 +0200

    Style: Clarify that you cannot create an event if you are not logged in

--- app/components/eventsComponent.tsx
+++ app/components/eventsComponent.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import Link from "next/link";
-import Events from "../components/eventsList";
 import style from "../ui/events.module.css";
 
-export default async function EventsHeader({ idUser }: { idUser: number }) {
+export default function EventsHeader({ idUser }: { idUser: number }) {
 
   return (
     <>
     <header className={style.header}>
     <h1 className={style.h1}>Explore events</h1>
-    {idUser !== 0 ? <Link href="/events/create" className={style.link}>Create a event</Link> : null}
+    {idUser !== 0 ? <Link href="/events/create" className={style.link}>Create a event</Link> : 
+    <p className={style.link}>Log in to create an event</p>}
     </header>
     </>
   );
